08308fa45bb62b740a806a03abd6530a825e3322
saidz <m.masoud247@gmail.com>
2022-03-12 14:22:51 +0100

    alarm view avec le bon titre et description

--- app/src/main/java/com/myplanner/myplanner/controllers/AlarmActivity.java
+++ app/src/main/java/com/myplanner/myplanner/controllers/AlarmActivity.java
@@ -1,8 +1,11 @@
 package com.myplanner.myplanner.controllers;
 
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +14,28 @@ import com.myplanner.myplanner.R;
 
 public class AlarmActivity extends AppCompatActivity {
 
+    Button btnFermer;
+    TextView titreView, descView;
+
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+        btnFermer = findViewById(R.id.btnFermer);
+        titreView = findViewById(R.id.titre);
+        descView = findViewById(R.id.description);
+
+        Intent intent = getIntent();
+        String titre = intent.getStringExtra("titre");
+        String description = intent.getStringExtra("description");
+        titreView.setText(titre);
+        descView.setText(description);
+
+        btnFermer.setOnClickListener(view -> {
+            startActivity(new Intent(this, AccueilActivity.class));
+            finish();
+        });
+
     }
 }
